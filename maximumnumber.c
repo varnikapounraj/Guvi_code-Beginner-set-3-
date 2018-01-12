f0c0628f6bf8d860067355f956701a8d421a8185
ano_anmol <anmolshukla7@gmail.com>
2018-01-12 09:00:57 +0530

    find maximum number in arrray
     Given a number N and an array of N integers,Print the maximum element
    Input Size : 1<= N <= 100000

---
+++ maximumnumber.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int a[1000],max=0,i,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+
+}
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(max<a[i])
+{
+max=a[i];
+}
+
+}
+printf("%d",max);
+
+
+
+}
